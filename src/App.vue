c51180ca85c49375366085bf9c1ba285f2900a31
geekape <861013016@qq.com>
2018-09-20 20:05:47 +0800

    侧边导航

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Index />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Index from './pages/Index.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Index
   }
 }
 </script>
@@ -21,8 +20,12 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+html,body,#app {
+  width: 100%;
+  height: 100%;
+}
+ul,li {
+  list-style-type: none;
 }
 </style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="index">
+      <!-- 侧边栏 -->
+      <div class="side-menu">
+          <h2 class="side-menu__tt">极客猿梦导航</h2>
+
+          <ul class="side-menu__list">
+              <li v-for="item in menuList" class="side-menu__item">{{item}}</li>
+          </ul>
+      </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+    name: 'index',
+    data: function() {
+        return {
+            menuList: ['网络运营', '设计素材', '实用工具', '技术博客', '开发社区']
+        }
+    }
+}
+</script>
+
+
+
+<style lang="scss" scoped>
+
+
+.index,
+.side-menu {
+    height: 100%;
+}
+.side-menu {
+    position: fixed;
+    background: #30333c;
+    color: #6b7386;
+    box-sizing: border-box;
+    flex-direction: column;
+    display: flex;
+    width: 248px;
+    transition: all .5s;
+}
+.side-menu__tt {
+    color: #fff;
+    text-align: center;
+    font-weight: 500;
+    padding-bottom: 20px;
+    border-bottom: 2px solid rgba(255,255,255,.04);
+}
+.side-menu__list {
+
+}
+.side-menu__item {
+    padding: 15px 0;
+}
+</style>
+
+
+
